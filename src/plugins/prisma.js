a22e006d8cc13eee179c38a085e1b190b4124bb7
Harmandeep Singh <115776636+Harmandeep98@users.noreply.github.com>
2026-05-02 13:46:18 +0530

    feat: add Prisma plugin

---
+++ src/plugins/prisma.js
@@ -0,0 +1,15 @@
+import fp from 'fastify-plugin'
+import { PrismaClient } from '@prisma/client'
+
+async function prismaPlugin(fastify) {
+  const prisma = new PrismaClient()
+  await prisma.$connect()
+
+  fastify.decorate('prisma', prisma)
+
+  fastify.addHook('onClose', async () => {
+    await prisma.$disconnect()
+  })
+}
+
+export default fp(prismaPlugin, { name: 'prisma' })
